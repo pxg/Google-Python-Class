7a999f270912c2670ae4cabf593b9b6181383753
Pete Graham <petegraham1@gmail.com>
2012-04-16 22:17:20 +0100

    basic exercises done

--- basic/string1.py
+++ basic/string1.py
@@ -69,7 +69,9 @@ def fix_start(s):
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
   # +++your code here+++
-  return
+  a2 = a[2:] # a without fist 2 chars
+  b2 = b[2:] # b wihout first 2 chars
+  return b[:2] + a[2:] + ' ' + a[:2] + b[2:]
 
 
 # Provided simple test() function used in main() to print
