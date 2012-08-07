37124f48517e12ef06cccb60d692a97efb463c69
Pete X. Graham <pete.graham@piratalondon.com>
2012-08-07 17:09:14 +0100

    1st list exercise done

--- basic/list1.py
+++ basic/list1.py
@@ -22,7 +22,11 @@
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
   # +++your code here+++
-  return
+  count = 0
+  for word in words:
+    if len(word) >= 2 and word[0] == word[-1]:
+      count += 1
+  return count
 
 
 # B. front_x
